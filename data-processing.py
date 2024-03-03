481b065eb8c5517232884f7941a08640f05a5cb3
Cyrus Dinh <dmcksclck55@gmail.com>
2024-03-03 12:22:11 +0700

    add os

--- data-processing.py
+++ data-processing.py
@@ -2,6 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+import os
+
+
 # Load the data from the CSV file
 file_path = 'data/skintype_v3.csv'
 df = pd.read_csv(file_path)
@@ -21,8 +24,6 @@ plt.xlabel('Date and Time')
 plt.ylabel('Hydration Level')
 plt.xticks(rotation=45)
 plt.legend()
-plt.show()
-
 
 # Create a folder named 'visualizations' if it doesn't exist
 output_folder = 'visualizations'
@@ -31,3 +32,7 @@ os.makedirs(output_folder, exist_ok=True)
 # Save the image to the 'visualizations' folder
 output_path = os.path.join(output_folder, 'hydration_over_time.png')
 plt.savefig(output_path)
+
+plt.show()
+
+
