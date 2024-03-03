a172fee4dbc5ebed6ec5c3aca01ab22f6d2f32a2
Cyrus Dinh <dmcksclck55@gmail.com>
2024-03-03 11:32:56 +0700

    change code to fix bug out index

--- cosine_similarity-sklearn-rs.py
+++ cosine_similarity-sklearn-rs.py
@@ -3,7 +3,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 # Load the product and user data
 df_products = pd.read_csv("data/acie_product.csv")
-df_users = pd.read_csv("data/acie_user.csv")
+df_users = pd.read_csv("data/skincare.csv")
 
 # For simplicity, let's create a user-product interaction matrix with random interactions
 import numpy as np
@@ -19,10 +19,16 @@ item_similarity = cosine_similarity(df_interactions.T)
 
 # Function to get product recommendations for a given product
 def get_product_recommendations(product_id, item_similarity=item_similarity, num_recommendations=5):
-    product_idx = df_products[df_products['_id'] == product_id].index[0]
-    similar_scores = item_similarity[product_idx]
-    similar_products = list(df_products['_id'].iloc[np.argsort(similar_scores)[::-1][1:num_recommendations + 1]])
-    return df_products[df_products['_id'].isin(similar_products)]
+    product_idx = df_products[df_products['_id'] == product_id].index
+    if not product_idx.empty:
+        product_idx = product_idx[0]
+        similar_scores = item_similarity[product_idx]
+        similar_products = list(df_products['_id'].iloc[np.argsort(similar_scores)[::-1][1:num_recommendations + 1]])
+        return df_products[df_products['_id'].isin(similar_products)]
+    else:
+        print(f"Product ID {product_id} not found.")
+        return None
+
 
 # Example usage
 product_id = '60e9f52eb5363725ef87e1b4'  # Replace with an actual product ID from your dataset
